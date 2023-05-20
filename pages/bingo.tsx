5d0caa6c7c9168485589cf6cb2ffbe6f91054dee
ryouhei <@ryouhei_0705>
2023-05-20 13:51:58 +0900

    test

---
+++ pages/bingo.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const Bingo = () => {
+    const [board, setBoard] = useState([
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+    ]);
+
+    const handleClick = (row, col) => {
+        const updatedBoard = [...board];
+        updatedBoard[row][col] = 'X';
+        setBoard(updatedBoard);
+    };
+
+    return (
+        <div>
+            <h1>Bingo Game</h1>
+            <table>
+                <tbody>
+                    {board.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {row.map((cell, colIndex) => (
+                                <td key={colIndex} onClick={() => handleClick(rowIndex, colIndex)}>
+                                    {cell}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Bingo;
