c0324698c61fca28f5bc772f18cbcba49249594a
ryouhei <@ryouhei_0705>
2023-05-21 15:29:35 +0900

    new bingo new をnew bingoに

--- pages/bingo.tsx
+++ pages/bingo.tsx
@@ -119,7 +119,7 @@ const Bingo = () => {
                 className="mt-8 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600"
                 onClick={regenerateBoard}
             >
-                New Bingo new
+                New Bingo
             </button>
         </div>
     );
