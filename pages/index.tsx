b0ca141107de2249044655314e24ee9a72cd6999
ryouhei <@ryouhei_0705>
2023-05-21 14:32:35 +0900

    メタタグに追記

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Bingo from './bingo';
+import Head from 'next/head';
+
 
 const Home = () => {
   return (
     <div>
+      <Head>
+        <title>エンジョイスプラ</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Bingo />
     </div>
   );
