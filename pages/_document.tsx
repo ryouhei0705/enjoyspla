a2963a6ab2183187bdc38105e60bd86a5c2187cc
ryouhei <@ryouhei_0705>
2023-05-21 21:24:45 +0900

    crossOrigin o->O

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3691680629047240" crossorigin="anonymous"></script>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3691680629047240" crossOrigin="anonymous"></script>
       <body>
         <Main />
         <NextScript />
