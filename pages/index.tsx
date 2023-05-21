b9c99d58f01409ec5d974b7c9fe1d7f3c4ca8946
ryouhei <@ryouhei_0705>
2023-05-21 21:19:49 +0900

    ハンバーガーメニュー実装できてない

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Bingo from './bingo';
 import Head from 'next/head';
-
+import HamburgerMenu from '../components/HamburgerMenu'
 
 const Home = () => {
   return (
     <div>
+      <HamburgerMenu />
       <Head>
         <title>エンジョイスプラ</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+
       <Bingo />
     </div>
   );
