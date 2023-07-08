e4a148f0bc752147d657d4bd875cbd27b3f84fc1
ryouhei <@ryouhei_0705>
2023-07-08 17:29:00 +0900

    下部への著作権表示と上部へのナビゲーションの追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3691680629047240" crossOrigin="anonymous"></script>
+      
+      <header>
+        <h1>エンジョイスプラ</h1>
+        <nav>
+          <ul>
+            <li><Link href='/'>スプラビンゴへ</Link></li>
+            <li><Link href='/about'>管理者紹介</Link></li>
+          </ul>
+        </nav>
+      </header>
+
       <body>
         <Main />
         <NextScript />
       </body>
+
+      <footer>
+        <p>&copy; copyright enjoyspla 2023</p>
+      </footer>
     </Html>
   )
 }
