137a30a8154cc1bad91d020809617ccec861de3b
ryouhei <@ryouhei_0705>
2023-07-08 17:29:24 +0900

    aboutページの中身を充実化

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import styles from '@/styles/about.module.scss'
 
 const About = () => {
     return (
@@ -13,11 +14,27 @@ const About = () => {
             <Link href='/'>
                 スプラビンゴへ
             </Link>
-            <div>
-                <p>
-                    このサイトは<a href="https://twitter.com/ryouhei_0705">@ryouhei0705</a>が運営しています。また、Amazonのアソシエイトとして、ryouhei0705は適格販売により収入を得ています。
-                </p>
+
+            <div className={styles.subtitle}>
+                サイト管理者
             </div>
+            <div>
+                ryouhei0705
+            </div>
+
+            <div className={styles.subtitle}>
+                ツイッターアカウント
+            </div>
+            <div>
+                <a href="https://twitter.com/ryouhei_0705">@ryouhei0705</a>
+            </div>
+
+            <div>
+                アマゾンリンク
+                <br></br>
+                エンジョイスプラ及びその管理者であるryouhei0705は、Amazonのアソシエイトとして適格販売により収入を得ています。
+            </div>
+       
         </div>
     );
 };
